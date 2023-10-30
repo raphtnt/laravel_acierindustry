17bbdb752eb2208f36e95600169948cc5a81ea84
RaphTNT <raphtnt@gmail.com>
2023-10-30 16:59:10 +0100

    remove searchbar in run-table and orderby created_at desc

--- oilcompagny/resources/views/livewire/run-table.blade.php
+++ oilcompagny/resources/views/livewire/run-table.blade.php
@@ -224,27 +224,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg" style="padding: 2rem;">
-                <p>Listes des derniers trajets</p>
+                <p style="margin-bottom: 0.75rem;">Listes des derniers trajets</p>
 
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <div class="flex items-center justify-between pb-4 bg-white dark:bg-gray-900">
-
-                        <div>
-                            <button type="button" class="text-white bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" x-show="selection.length > 0" x-on:click="$wire.cancel(selection)">Annulé</button>
-                        </div>
-
-                        <label for="table-search" class="sr-only">Search</label>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                </svg>
-                            </div>
-                            <input type="text" id="table-search-users" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users">
-                        </div>
-                    </div>
-
-                    <span x-html="JSON.stringify(selection)"></span>
 
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
